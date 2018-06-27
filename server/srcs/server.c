4b6629df69704ba743bdb99736db15cfe402f148
Clovis DEROUCK <derouc_c@etna-alternance.net>
2018-06-27 15:09:57 +0200

    WIP connectable server

--- server/srcs/server.c
+++ server/srcs/server.c
@@ -1,1 +1,14 @@
-#include    "../includes/server.h"
+#include	"../includes/server.h"
+
+int		manage_server(ARGS *arguments) {
+  (void) arguments;
+  // prepare_resources();
+  if (start_server(arguments) != 0)
+    return 1;
+  return 0;
+}
+
+int		start_server(ARGS *arguments) {
+  (void) arguments;
+  return 0;
+}
