2bb964d29a371c0ad96484828705839e78dc2ebb
Epillepsy <gueye_o@etna-alternance.net>
2018-08-07 19:39:40 +0200

    remove useless code

--- client/src/serverUtil.js
+++ client/src/serverUtil.js
@@ -1,13 +1,9 @@
-
 export const parseResponse = (response) => {
   const res = response.split('|');
   return { status: res[0], data: res[1] };
 };
 
-export const parseCommand = (command, argument = null) => {
-  const data = argument || null;
-  return `${command}|${data}`;
-};
+export const parseCommand = (command, argument = null) => `${command}|${argument}`;
 
 export const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
