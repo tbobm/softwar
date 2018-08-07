751fac15687c02ff40c2d126be7574f020e70b74
Clovis DEROUCK <derouc_c@etna-alternance.net>
2018-08-07 14:00:01 -0400

    Removed not needed things

--- client_test/conf/jest.config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    rootDir: path.join(process.cwd(), 'src'),
-    collectCoverage: true,
-    verbose: true
-};
--- client_test/src/Energy.js
+++
@@ -1,9 +0,0 @@
-
-import Drawable from './Drawable';
-
-export default class Energy extends Drawable {
-  constuctor(drawableKit, value) {
-    super({ ...drawableKit });
-    this.value = value;
-  }
-}
