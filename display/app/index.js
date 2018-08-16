70f636920b2eb1c190c7787cc5d86cbce56feb6e
Epillepsy <gueye_o@etna-alternance.net>
2018-08-16 16:47:00 +0200

    add class

--- display/app/index.js
+++ display/app/index.js
@@ -1,28 +1,36 @@
-/**
- * Application entry point
- */
-
-// Load application styles
-import 'styles/index.scss';
 import MainLoop from 'mainloop.js';
-import * as Pixi from 'pixi.js';
+import * as PIXI from 'pixi.js';
+
+import 'styles/index.scss';
+import data from './Data';
+import { createGrid } from './mapHandler';
+import Player from './Player';
+
+const size = 500;
+const tileSize = size / data.GameInfo.map_size;
+const debug = true;
+const app = new PIXI.Application({ width: size, height: size });
+
+const initPlayerFromData = ({ GameInfo }, tileSize) => {
+  const { players } = GameInfo;
+  let playersClass = {};
+  players.map((player) => {
+    playersClass[player.name] = new Player(player, tileSize, "");
+  })
+  return playersClass;
+};
 
 const update = () => {
-  rectangle.x += 1;
-  rectangle.y += 1;
+  if (debug)
+    document.getElementById("fps").innerHTML = `fps: ${MainLoop.getFPS()}`;
 }
-MainLoop.setUpdate(update).start();
-document.getElementById("fps").innerHTML = MainLoop.getFPS(); 
 
-const app = new PIXI.Application({width: 500, height: 500});
+const initGame = (app) => {
+  createGrid(data, app, tileSize);
+  const players = initPlayerFromData(data);
+  console.log(players);
+  document.body.appendChild(app.view);
+  MainLoop.setUpdate(update).start();
+}
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM
-document.body.appendChild(app.view);
-
-// load the texture we need
-let rectangle = new PIXI.Graphics();
-rectangle.beginFill(0x66CCFF);
-rectangle.drawRect(0, 0, 64, 64);
-rectangle.endFill();
-app.stage.addChild(rectangle);
+initGame(app);
