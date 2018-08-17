356ab1f939fd0b028c22919bc7d1fd895eee4902
Clovis DEROUCK <derouc_c@etna-alternance.net>
2018-08-17 10:57:07 -0400

    added selfid selfstats

--- server/srcs/server_RFC.c
+++ server/srcs/server_RFC.c
@@ -41,25 +41,35 @@ zframe_t 		*backward(t_server_info *server_info)
 zframe_t 		*leftfwd(t_server_info *server_info)
 {
 	t_player 	*tmp;
+	zframe_t 	*res;
 
 	if ((tmp = search_by_name(server_info->game_info.list_players, server_info->identity)) == NULL) {
 		return zframe_from("KO|identity unknown");
 	}
 	printf("Rotating left, then moving '%s' forward.\n", server_info->identity);
     rotate_left(tmp);
-    return go_forward(server_info, tmp);
+    res = go_forward(server_info, tmp);
+    if (zframe_strdup(res)[0] == 'K') {
+    	rotate_right(tmp);
+    }
+    return res;
 }
 
 zframe_t 		*rightfwd(t_server_info *server_info)
 {
 	t_player 	*tmp;
+	zframe_t 	*res;
 
 	if ((tmp = search_by_name(server_info->game_info.list_players, server_info->identity)) == NULL) {
 		return zframe_from("KO|identity unknown");
 	}
 	printf("Rotating right, then moving '%s' forward.\n", server_info->identity);
     rotate_right(tmp);
-    return go_forward(server_info, tmp);
+    res = go_forward(server_info, tmp);
+    if (zframe_strdup(res)[0] == 'K') {
+    	rotate_left(tmp);
+    }
+    return res;
 }
 
 zframe_t 		*right(t_server_info *server_info)
@@ -116,14 +126,28 @@ zframe_t 		*attack(t_server_info *server_info)
 
 zframe_t 		*selfid(t_server_info *server_info)
 {
-	(void)server_info;
-	return zframe_from("OK|Selfid");
+	t_player 	*tmp;
+
+	if ((tmp = search_by_name(server_info->game_info.list_players, server_info->identity)) == NULL) {
+		return zframe_from("KO|identity unknown");
+	}
+
+	char 		buff[strlen(tmp->name + 3)];
+	
+	sprintf(buff, "OK|%s", tmp->name);
+	return zframe_from(buff);
 }
 
 zframe_t 		*selfstats(t_server_info *server_info)
 {
-	(void)server_info;
-	return zframe_from("OK|Selfstats");
+	t_player 	*tmp;
+	char 		buff[7];
+
+	if ((tmp = search_by_name(server_info->game_info.list_players, server_info->identity)) == NULL) {
+		return zframe_from("KO|identity unknown");
+	}
+	sprintf(buff, "OK|%d", tmp->energy);
+	return zframe_from(buff);
 }
 
 zframe_t 		*inspect(t_server_info *server_info)
